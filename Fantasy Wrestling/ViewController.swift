25638984d4af60c4211114e7be5f469af3b42bab
Tarang <tsgoten@gmail.com>
2018-10-04 11:55:59 -0400

    Initial Commit

---
+++ Fantasy Wrestling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fantasy Wrestling
+//
+//  Created by Tarang Srivastava on 10/4/18.
+//  Copyright © 2018 Tarang Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
